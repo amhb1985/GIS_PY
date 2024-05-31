ebba7304cbd6f31bfee4ebc10523b607dd5f460a
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-31 15:10:47 +0000

    Arc_py_description02

--- shaply copy 3.py
+++ shaply copy 3.py
@@ -1,1 +1,10 @@
 # this is about ArcPy under arcMap pro3:
+
+#for the first of project we need tahat we active or import the Arcpy package inside o dthe our python terminal in the
+#Arc pro.
+
+#Notice that we needed brfore the python package that must be installed inside of Progrom:
+
+#1. arcpy
+#.1.1 we can see that here we  script its not recognise 
+import arcpy
