432887bff3d2a2db166d907e0ba7dffd04da99a0
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-02 07:28:25 +0000

    arcpy_test_insertLayer

--- arcpy_03.py
+++ arcpy_03.py
@@ -6,13 +6,17 @@ import arcpy
 # Define the path to your project and the feature layer
 project_path = r"C:\Users\amir\Documents\ArcGIS\Projects\Arypy_project\Arypy_project.aprx"
 
-
 # Change this to the name of your layer
+# we have inside of our test arcpy project 3 different Layers(Point, Line, Insel) we use one of them:
+ayername =  "Point"
+
 
 # Open the project
 aprx = arcpy.mp.ArcGISProject(project_path)
 
 # Access the first map in the project (modify this if you have multiple maps)
+#inside of parallel we have another problem that with [] i cannot find the exact Keyborad and i must ust the google translator to insetr it with the click of Mouse instead of Keyboard.
+
 map = aprx.listMaps()[0]
 
 # Access the layer by name
