ff8f9d7835ab407f851e17ae80522100824b6640
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-04 14:02:58 +0000

    scripte_for_save_as_text

--- arcpy_04.py
+++ arcpy_04.py
@@ -1,48 +1,74 @@
-##this is the secound script but now for the Python IDEL bcos we want the result as text
-#in our desktop
+#. so now we can see the result of the script inside of the our scripte
 
 import arcpy
 import os
 
 # Define the path to your project and the feature layer
-project_path = r"C:\Users\amir\Documents\ArcGIS\Projects\Arypy_project\Arypy_project.aprx"
+project_path = r"C:\Users\amir\Documents\ArcGIS\Projects\MyProject_test170424\MyProject_test170424.aprx"
 
-# Change this to the name of your layer
-layer_name = "Point"
+# Specify the map name explicitly
+map_name = "Map"  # Change this to the actual map name if different
+layer_name = "Baum_Clip_Weg"
 
 # Define the output text file path
-output_file_path = os.path.join(os.path.expanduser("~"), "Desktop", "attribute_table.txt")
+output_file_path = r"C:\Users\amir\Desktop\attribute_table.txt"
 
 # Open the project
+print(f"Opening project: {project_path}")
 aprx = arcpy.mp.ArcGISProject(project_path)
 
-# Access the first map in the project (modify this if you have multiple maps)
-map = aprx.listMaps()[0]
+# Access the map by name
+print(f"Accessing map: {map_name}")
+maps = aprx.listMaps(map_name)
+if not maps:
+    raise Exception(f"Map '{map_name}' not found in the project.")
+else:
+    print(f"Found map: {map_name}")
+
+# There should be only one map with the name, if not, this will handle the first match
+map_obj = maps[0]
+
+# List all layers in the map for debugging
+print("Available layers in the map:")
+for lyr in map_obj.listLayers():
+    print(f"Layer Name: {lyr.name}, Layer Type: {'Feature Layer' if lyr.isFeatureLayer else 'Other'}")
 
 # Access the layer by name
-layer = map.listLayers(layer_name)[0]
+print(f"Accessing layer: {layer_name}")
+layers = map_obj.listLayers(layer_name)
+if len(layers) == 0:
+    raise Exception(f"Layer '{layer_name}' not found in the map.")
+else:
+    print(f"Found layer: {layer_name}")
+layer = layers[0]
 
 # Check if the layer is a feature layer
-if layer.isFeatureLayer:
-    # Get the path to the layer's data source
-    data_source = layer.dataSource
+print(f"Checking if layer '{layer_name}' is a feature layer")
+if not layer.isFeatureLayer:
+    raise Exception(f"Layer '{layer_name}' is not a feature layer.")
+else:
+    print(f"Layer '{layer_name}' is a feature layer")
 
-    # Open a search cursor to read through the attribute table
-    with arcpy.da.SearchCursor(data_source, ["*"]) as cursor:
-        # Open the output file in write mode
-        with open(output_file_path, 'w') as file:
-            # Write the header (field names)
-            field_names = [field.name for field in arcpy.ListFields(data_source)]
-            file.write("\t".join(field_names) + "\n")
+# Get the path to the layer's data source
+data_source = layer.dataSource
+print(f"Data source for layer '{layer_name}': {data_source}")
 
-            # Write each row of the attribute table to the file
-            for row in cursor:
-                file.write("\t".join(map(str, row)) + "\n")
+# Open a search cursor to read through the attribute table
+print(f"Reading attribute table for layer '{layer_name}'")
+with arcpy.da.SearchCursor(data_source, ["*"]) as cursor:
+    # Open the output file in write mode
+    print(f"Writing attribute table to file: {output_file_path}")
+    with open(output_file_path, 'w') as file:
+        # Write the header (field names)
+        field_names = [field.name for field in arcpy.ListFields(data_source)]
+        file.write("\t".join(field_names) + "\n")
 
-    print(f"Attribute table saved to {output_file_path}")
+        # Write each row of the attribute table to the file
+        for row in cursor:
+            file.write("\t".join(map(str, row)) + "\n")
+
+print(f"Attribute table saved to {output_file_path}")
 
 # Clean up
 del aprx
 
-
-#test again inside of arcpro with another project
