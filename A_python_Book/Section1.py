827b83f29025b07616167d37dd408deabc161da9
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-07-03 13:59:26 +0000

    set_first_text

---
+++ A_python_Book/Section1.py
@@ -0,0 +1,26 @@
+''''
+This is a file for test an learning About the book  A Python Primer for ArcGIS by Nathan Jennungs.
+
+for more info an Demos as wrote in the Book we can use this authors Website at:
+
+www.jenningsplanet.com/PythonPrimer
+
+
+I dont have any ArcGIS Map but i have another Version as Arc Pro 3
+
+we can use this user Name and Password for Website:
+
+Username: 0PythonPrimer0
+
+Password: 0PP4AGIS!0
+
+Requierd Software is 
+ESRI Softwares :ArcView, ArcEditor, ArcInfo 
+(I wann try use ArcPro 3 )
+
+Python : Upper (Up) 2.6.5
+'''
+
+
+
+
